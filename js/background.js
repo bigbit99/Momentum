6d12b7c08f2d7246112a80b5fc30fbf8945e4fdb
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-09-02 18:24:48 +0900

    배경이미지

---
+++ js/background.js
@@ -0,0 +1,18 @@
+const images = [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "06.jpg",
+    "07.jpg",
+]
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+//appendChild()는 body에 html을 추가함 
